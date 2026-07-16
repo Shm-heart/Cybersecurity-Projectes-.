ecf22465f59f4c3244cd49f057d2e169e46dd4af
Shm-heart <shanmuganshanmugan920@gmail.com>
2026-07-16 19:00:15 +0530

    add the python project

--- password-checking-using-python/pass-check.py
+++ password-checking-using-python/pass-check.py
@@ -1,13 +1,29 @@
 from colorama import Fore, init ,Style
 import time
 user_name=input("Enter a username : ")
-password=input("Enter a username : ")
+password=input("Enter a password : ")
 file=open("password.txt",'r')
+p=0
+p1=100
 for i in file:
-    if i.strip()==password.strip():
-        print(Fore.GREEN+f"the user password {password} was identfided  ")
+  
+    if i.strip()==password.strip():        
+        p+=100
+        percent = p+p1
+        per=percent/2
+        bar = "#" * (p // 2)
+        spaces = " " * (50 - len(bar))
+        print(Fore.GREEN+f"\r[{bar}{spaces}] {per}%", end="")
+        time.sleep(1)
+        print(Fore.GREEN+f"\nthe user password {password} was identfided  ")
         break
     else:
-        print(Fore.RED+f"\rTring..... to crake the password of user {password} using dummy password {i} ",end="")
+        print("\r",Fore.RED+"Craking...........",end="")
+        p+=1
+        p1-=1
+        percent = p
+        bar = "#" * (p // 2)
+        spaces = " " * (50 - len(bar))
+        print(f"\r[{bar}{spaces}] {percent}%", end="")
         time.sleep(1)
 Style.RESET_ALL
