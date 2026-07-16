73dac27917f24c08c5ff2cc5dee64445314a233c
Shm-heart <shanmuganshanmugan920@gmail.com>
2026-07-16 17:47:13 +0530

    add the python project

--- password-checking-using-python/pass-check.py
+++ password-checking-using-python/pass-check.py
@@ -5,7 +5,7 @@ password=input("Enter a username : ")
 file=open("password.txt",'r')
 for i in file:
     if i.strip()==password.strip():
-        print(Fore.GREEN+f"the user password {password} was craked  ")
+        print(Fore.GREEN+f"the user password {password} was identfided  ")
         break
     else:
         print(Fore.RED+f"\rTring..... to crake the password of user {password} using dummy password {i} ",end="")
